e5b38a38601d6d5b11cff3b7c19d9541256ab39d
James Ward <james@jamesward.com>
2019-09-24 04:59:52 -0600

    external

---
+++ .gcloud/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "project id: $PROJECT_ID"
+echo "service name: $SERVICE"
+echo "region: $REGION"
+
+echo -n "now enter something: "
+read something
+echo "you entered: $something"
